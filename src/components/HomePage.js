8ea0ddb24a281fad814e3d531a7bc303877e84c8
Armin <armin.schweighofer@creamfinance.com>
2019-09-27 14:11:06 +0200

    Added basic Routing and about page

---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div className='jumbotron'>
+            <h1>Homepage</h1>
+            <p>Lorem ipsum</p>
+            <a href='/about'>About</a>
+        </div>
+    );
+};
+
+export default HomePage;
---
+++ src/components/common/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <nav>
+            <a href='/'>Home</a> | <a href='/about'>About</a>
+        </nav>
+    );
+};
+
+export default Header;
