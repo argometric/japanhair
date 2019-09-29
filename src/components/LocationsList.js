14186198e1c9ca7e9c4336cd4d7c8131ed2e71d6
armin schweighofer <arminschweighofer@hotmail.com>
2019-09-29 23:30:17 +0200

    Fixed React-Key error

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -8,30 +8,28 @@ const LocationsList = props => {
           <div className="row">
             {props.locations.map(locations => {
               return (
-                <>
-                  <div className="col-md-4">
-                    <div className="card mb-4 shadow-sm" key={locations.id}>
-                      <img
-                        className="bd-placeholder-img card-img-top"
-                        width="100%"
-                        height="225"
-                        style={{ objectFit: "cover" }}
-                        src={locations.imgUrl}
-                        alt={locations.name}
-                      />
-                      <div className="card-body">
-                        <div className="card-text text-center">
-                          <h1>{locations.name}</h1>
-                          <h2>{locations.phone}</h2>
-                          <h2>{locations.email}</h2>
-                          <button className="btn btn-dark btn-lg">
-                            Book Now
-                          </button>
-                        </div>
+                <div className="col-md-4" key={locations.id}>
+                  <div className="card mb-4 shadow-sm">
+                    <img
+                      className="bd-placeholder-img card-img-top"
+                      width="100%"
+                      height="225"
+                      style={{ objectFit: "cover" }}
+                      src={locations.imgUrl}
+                      alt={locations.name}
+                    />
+                    <div className="card-body">
+                      <div className="card-text text-center">
+                        <h1>{locations.name}</h1>
+                        <h2>{locations.phone}</h2>
+                        <h2>{locations.email}</h2>
+                        <button className="btn btn-dark btn-lg">
+                          Book Now
+                        </button>
                       </div>
                     </div>
                   </div>
-                </>
+                </div>
               );
             })}
           </div>
