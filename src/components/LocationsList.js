30dbf9240b0fcf669b77386d0975cd81388f74e8
armin schweighofer <arminschweighofer@hotmail.com>
2019-10-01 22:43:45 +0200

    Added Material-UI

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,42 +1,71 @@
 import React from "react";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import { makeStyles } from "@material-ui/core/styles";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
-const LocationsList = props => {
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1),
+    backgroundColor: "#2E2E2E",
+    color: "white",
+    "&:hover": {
+      backgroundColor: "#212121"
+    }
+  }
+}));
+
+export default function LocationsList(props) {
+  const classes = useStyles();
   return (
-    <>
+    <div className="container">
       <div className="album py-2">
-        <div className="container">
-          <div className="row">
-            {props.locations.map(locations => {
-              return (
-                <div className="col-md-4" key={locations.id}>
-                  <div className="card mb-4 shadow-sm">
-                    <img
-                      className="bd-placeholder-img card-img-top"
-                      width="100%"
-                      height="225"
-                      style={{ objectFit: "cover" }}
-                      src={locations.imgUrl}
-                      alt={locations.name}
+        <div className="row">
+          {props.locations.map(locations => {
+            return (
+              <div className="col-md-4">
+                <Card className="card mb-4 shadow-sm">
+                  <CardActionArea>
+                    <CardMedia
+                      image={locations.imgUrl}
+                      style={{ height: 0, paddingTop: "56.25%" }}
+                      title={locations.name}
                     />
-                    <div className="card-body">
-                      <div className="card-text text-center">
-                        <h1>{locations.name}</h1>
-                        <h2>{locations.phone}</h2>
-                        <h2>{locations.email}</h2>
-                        <button className="btn btn-dark btn-lg" id="orangeBtn">
-                          Book Now
-                        </button>
-                      </div>
-                    </div>
+                    <CardContent>
+                      <Typography
+                        gutterBottom
+                        variant="h3"
+                        component="h2"
+                        className="text-center"
+                      >
+                        {locations.name}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                  <div className="text-center">
+                    <h2>{locations.phone}</h2>
+                    <h2>{locations.email}</h2>
                   </div>
-                </div>
-              );
-            })}
-          </div>
+                  <CardActions style={{ justifyContent: "center" }}>
+                    <Button
+                      variant="contained"
+                      className={classes.button}
+                      id="orangeBtn"
+                      size="large"
+                    >
+                      Book Now
+                    </Button>
+                  </CardActions>
+                </Card>
+              </div>
+            );
+          })}
         </div>
       </div>
-    </>
+    </div>
   );
-};
-
-export default LocationsList;
+}
