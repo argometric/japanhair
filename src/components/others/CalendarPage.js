e034f5b2a5c76855068875fcb8e659a106dd098e
armin schweighofer <arminschweighofer@hotmail.com>
2019-10-14 01:37:19 +0200

    added table rendering

--- src/components/others/CalendarPage.js
+++ src/components/others/CalendarPage.js
@@ -1,12 +1,5 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
-import { TextField } from "@material-ui/core";
 
 function createData(name, calories, fat, carbs, protein) {
   return { name, calories, fat, carbs, protein };
@@ -34,7 +27,9 @@ const useStyles = makeStyles(theme => ({
     overflowX: "auto"
   },
   table: {
-    minWidth: 650
+    width: "100%",
+    border: "none",
+    borderCollapse: "collapse"
   }
 }));
 
@@ -42,36 +37,37 @@ const CalendarPage = () => {
   const classes = useStyles();
   return (
     <>
-      <Paper className={classes.root}>
-        <Table className={classes.table} stickyHeader>
-          <TableHead>
-            <TableRow>
-              <TableCell>Hour</TableCell>
-              <TableCell align="center">Calories</TableCell>
-              <TableCell align="center">Fat&nbsp;(g)</TableCell>
-              <TableCell align="center">Carbs&nbsp;(g)</TableCell>
-              <TableCell align="center">Protein&nbsp;(g)</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody id="TimeTable">
-            {rows.map(row => (
-              <TableRow key={row.name}>
-                <TableCell component="th" scope="row">
-                  {row.name}
-                </TableCell>
-                <TableCell align="center">
-                  <TextField>{row.calories}</TextField>
-                </TableCell>
-                <TableCell align="center">{row.fat}</TableCell>
-                <TableCell align="center">
-                  <>{row.carbs}</>
-                </TableCell>
-                <TableCell align="center">{row.protein}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </Paper>
+      <table className={classes.table} stickyHeader>
+        <thead>
+          <tr>
+            <td>Hour</td>
+            <td align="center">Calories</td>
+            <td align="center">Fat&nbsp;(g)</td>
+            <td align="center">Carbs&nbsp;(g)</td>
+            <td align="center">Protein&nbsp;(g)</td>
+          </tr>
+        </thead>
+        <tbody id="TimeTable">
+          {rows.map(row => (
+            <tr key={row.name}>
+              <th component="th">{row.name}</th>
+              <td align="center">
+                <input type="text" value={row.calories}></input>
+              </td>
+              <td align="center">
+                <input type="text" value={row.fat}></input>
+              </td>
+              <td align="center">
+                <input type="text" value={row.carbs}></input>
+              </td>
+              <td align="center">
+                {" "}
+                <input type="text" value={row.protein}></input>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 };
