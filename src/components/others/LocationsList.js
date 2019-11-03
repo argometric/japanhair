50adb4cbdea68f5410d7161db2145bfea2103a42
armin schweighofer <arminschweighofer@hotmail.com>
2019-11-04 00:16:26 +0100

    Added Gridlayout on homescreen

--- src/components/others/LocationsList.js
+++ src/components/others/LocationsList.js
@@ -12,8 +12,12 @@ import DatePickerClass from "../others/DatePickerClass";
 import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 import MomentUtils from "@date-io/moment";
 import { Link } from "react-router-dom";
+import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
   button: {
     margin: theme.spacing(1),
     backgroundColor: "#2E2E2E",
@@ -36,140 +40,128 @@ export default function LocationsList(props) {
   const handleClose = () => {
     setOpen(false);
   };
-  // const handleTabChange = val => {
-  //   localStorage.setItem("tabState", val);
-  // };
 
   return (
-    <div className="container">
-      <div className="album py-2">
-        <div className="row">
-          {props.locations.map(locations => {
-            return (
-              <div className="col-md-4" key={locations.id}>
-                <Card className="card mb-4 shadow-sm">
-                  <CardActionArea
+    <div className={classes.root}>
+      <Grid container spacing={5} direction="row" justify="space-evenly">
+        {props.locations.map(locations => {
+          return (
+            <Grid key={locations.id} item md>
+              <Card className="card mb-4 shadow-sm">
+                <CardActionArea
+                  onClick={() => {
+                    handleClickOpen();
+                    setLocation(locations.name);
+                  }}
+                >
+                  <CardMedia
+                    image={locations.imgUrl}
+                    style={{ height: 0, paddingTop: "56.25%" }}
+                    title={locations.name}
+                  />
+                  <CardContent>
+                    <Typography
+                      gutterBottom
+                      variant="h4"
+                      component="h2"
+                      className="text-center"
+                    >
+                      {locations.name}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+
+                <CardActions style={{ justifyContent: "center" }}>
+                  <Button
+                    className={classes.button}
+                    id="orangeBtn"
+                    size="large"
                     onClick={() => {
                       handleClickOpen();
                       setLocation(locations.name);
                     }}
                   >
-                    <CardMedia
-                      image={locations.imgUrl}
-                      style={{ height: 0, paddingTop: "56.25%" }}
-                      title={locations.name}
-                    />
-                    <CardContent>
-                      <Typography
-                        gutterBottom
-                        variant="h4"
-                        component="h2"
-                        className="text-center"
-                      >
-                        {locations.name}
-                      </Typography>
-                    </CardContent>
-                  </CardActionArea>
-
-                  <CardActions style={{ justifyContent: "center" }}>
-                    <Button
-                      className={classes.button}
-                      id="orangeBtn"
-                      size="large"
-                      onClick={() => {
-                        handleClickOpen();
-                        setLocation(locations.name);
-                      }}
-                    >
-                      Book now
-                    </Button>
-                  </CardActions>
-                  <Dialog open={open} onClose={handleClose} maxWidth="lg">
-                    <div
-                      className="flex-row px-4 justify-content-center"
-                      id="popUp"
-                    >
-                      {props.shops.map(shops => {
-                        if (location === shops.city) {
-                          return (
-                            <div className="py-4 px-3" key={shops.id}>
-                              <Card
-                                className="card shadow-sm"
-                                id="dialogCard"
-                                key={shops.id}
+                    Book now
+                  </Button>
+                </CardActions>
+                <Dialog open={open} onClose={handleClose} maxWidth="lg">
+                  <div
+                    className="flex-row px-4 justify-content-center"
+                    id="popUp"
+                  >
+                    {props.shops.map(shops => {
+                      if (location === shops.city) {
+                        return (
+                          <div className="py-4 px-3" key={shops.id}>
+                            <Card
+                              className="card shadow-sm"
+                              id="dialogCard"
+                              key={shops.id}
+                            >
+                              <CardActionArea
+                                component={Link}
+                                to={{
+                                  pathname: "/book",
+                                  shopname: shops.name
+                                }}
                               >
-                                <CardActionArea
+                                <CardMedia
+                                  image={shops.imgUrl}
+                                  style={{
+                                    height: 0,
+                                    paddingTop: "56.25%"
+                                  }}
+                                  title={shops.name}
+                                />
+                                <CardContent>
+                                  <Typography
+                                    gutterBottom
+                                    variant="h4"
+                                    component="h2"
+                                    className="text-center"
+                                  >
+                                    {shops.name}
+                                  </Typography>
+                                </CardContent>
+                              </CardActionArea>
+
+                              <CardActions style={{ justifyContent: "center" }}>
+                                <Button
+                                  className={classes.button}
+                                  id="orangeBtn"
+                                  size="large"
+                                >
+                                  <MuiPickersUtilsProvider utils={MomentUtils}>
+                                    <DatePickerClass />
+                                  </MuiPickersUtilsProvider>
+                                </Button>
+                                <Button
+                                  className={classes.button}
+                                  id="orangeBtn"
+                                  size="large"
                                   component={Link}
+                                  selected
                                   to={{
                                     pathname: "/book",
                                     shopname: shops.name
                                   }}
                                 >
-                                  <CardMedia
-                                    image={shops.imgUrl}
-                                    style={{
-                                      height: 0,
-                                      paddingTop: "56.25%"
-                                    }}
-                                    title={shops.name}
-                                  />
-                                  <CardContent>
-                                    <Typography
-                                      gutterBottom
-                                      variant="h4"
-                                      component="h2"
-                                      className="text-center"
-                                    >
-                                      {shops.name}
-                                    </Typography>
-                                  </CardContent>
-                                </CardActionArea>
-
-                                <CardActions
-                                  style={{ justifyContent: "center" }}
-                                >
-                                  <Button
-                                    className={classes.button}
-                                    id="orangeBtn"
-                                    size="large"
-                                  >
-                                    <MuiPickersUtilsProvider
-                                      utils={MomentUtils}
-                                    >
-                                      <DatePickerClass />
-                                    </MuiPickersUtilsProvider>
-                                  </Button>
-                                  <Button
-                                    className={classes.button}
-                                    id="orangeBtn"
-                                    size="large"
-                                    component={Link}
-                                    selected
-                                    to={{
-                                      pathname: "/book",
-                                      shopname: shops.name
-                                    }}
-                                    // onClick={() => {
-                                    //   handleTabChange(3);
-                                    // }}
-                                  >
-                                    Book
-                                  </Button>
-                                </CardActions>
-                              </Card>
-                            </div>
-                          );
-                        }
-                        return null;
-                      })}
-                    </div>
-                  </Dialog>
-                </Card>
-              </div>
-            );
-          })}
-        </div>
-      </div>
+                                  Book
+                                </Button>
+                              </CardActions>
+                            </Card>
+                          </div>
+                        );
+                      }
+                      return null;
+                    })}
+                  </div>
+                </Dialog>
+              </Card>
+            </Grid>
+          );
+        })}
+      </Grid>
     </div>
   );
 }
